179493b1b70fe86dc63c15dc82053bee938343a7
NightMare <rahmanishfaq146@gmail.com>
2024-10-09 15:12:25 +0500

    react startup template

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-
 import './App.css'
-import { lazy,Suspense } from 'react'
-import About from './components/About'
-import Loader from './components/loader/Loader'
-
-const Home = lazy(()=>import('./components/Home'))
 
 function App() {
   
 
   return (
     <>
-     <h1>hey i am lazy loading</h1>
-     <Loader/>
-     <Suspense fallback=<Loader/>>
-      <Home/>
-     </Suspense>
+     <h1>hey i am react startup template</h1>
+    
     </>
   )
 }
